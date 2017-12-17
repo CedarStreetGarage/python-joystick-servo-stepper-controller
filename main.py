133c5a261fc41482a31b623b5728c3d6daf23be0
Colin Shaw <colin.shaw@aya.yale.edu>
2017-12-17 07:47:41 -0600

    Changed argument parsing

--- main.py
+++ main.py
@@ -8,22 +8,20 @@ from   src.joint_control import JointControl
 
 p = argparse.ArgumentParser()
 
-p.add_argument('-j', '--joints',    action='store_true', help='Control joint angles')
-p.add_argument('-c', '--cartesian', action='store_true', help='Control Cartesian coordinates')
-p.add_argument('type', help='Maestro or Norberg')
+p.add_argument('-j', '--joints',    action='store_true', help='control joint angles')
+p.add_argument('-c', '--cartesian', action='store_true', help='control Cartesian coordinates')
+p.add_argument('-m', '--maestro',   action='store_true', help='use Maestro servo controller')
+p.add_argument('-n', '--norberg',   action='store_true', help='use Norberg SD6DX controller')
 
 args = p.parse_args()
 
-if args.type == 'Maestro':
-    if args.joints:
-        JointControl(Maestro()).go()
-    if args.cartesian:
-        pass
-
-if args.type == 'Norberg':
-    if args.joints:
-        JointControl(Norberg()).go()
-    if args.cartesian:
-        pass
+if args.joints and args.maestro:
+    JointControl(Maestro()).go()
+if args.joints and args.norberg:
+    JointControl(Norberg()).go()
+if args.cartesian and args.maestro:
+    pass
+if args.cartesian and args.norberg:
+    pass
 
 p.print_help()
